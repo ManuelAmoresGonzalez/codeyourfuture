38d9c1836de1a8888b5b7d113a12036930e518b5
ManuelAmoresGonzalez <amoresgonzalezmanuel@gmail.com>
2022-11-02 12:08:31 -0600

    Add WeSplit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,145 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Manuel Enrique Amores Gonzalez on 31/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    
+    let tipPercenteges = [10,15,20,25,0]
+    
+    var totalPerPerson: (splitAccount: Double, totalAmount: Double) {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return (amountPerPerson, grandTotal)
+    }
+    
+    var currencyFormatter: String{
+        return Locale.current.currencyCode ?? "USD"
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: currencyFormatter))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section{
+                    Text("How much tip do you want to leave ?")
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(0..<101, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                }header: {
+                    Text("How much tip do you want to leave?")
+                }
+                Section{
+                    Text(totalPerPerson.splitAccount, format: .currency(code: currencyFormatter))
+                }header: {
+                    Text("Price per Person")
+                }
+                
+                Section{
+                    Text(totalPerPerson.totalAmount, format: .currency(code: currencyFormatter))
+                }header: {
+                    Text("Price per Person")
+                }
+                
+            }.navigationTitle("WeSplit")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+
+/*
+ struct ContentView: View {
+     let students = ["Harry", "Hermione", "Ron"]
+     @State private var selectedStudent = "Harry"
+     var body: some View {
+         Form{
+             Picker("Selected your student", selection: $selectedStudent){
+                 
+                 ForEach(students, id: \.self){
+                     Text($0)
+                 }
+             }
+         }
+     }
+ }
+ */
+
+/*
+ struct ContentView: View {
+     @State private var name = ""
+     var body: some View {
+         Form{
+             TextField("Enter your name", text: $name)
+             Text("Your name is \(name)")
+         }
+     }
+ }
+ */
+
+
+/*
+struct ContentView: View {
+    @State var tapCount = 0
+    var body: some View {
+        Button("Tap Count: \(tapCount)"){
+            tapCount += 1
+        }
+        Text("Este: \(tapCount)")
+    }
+}*/
+
+
+/*struct ContentView: View {
+    var body: some View {
+        
+            NavigationView{
+                Form{
+                    Section{
+                        Text("Hello, world")
+                    }
+                }
+                .navigationTitle("SiwftUI")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+
+    }
+}*/
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Manuel Enrique Amores Gonzalez on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
